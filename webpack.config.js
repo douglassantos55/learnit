69c9517ed001de86723a0881b587573e53a08742
Douglas Santos <hiperhavy_doug@hotmail.com>
2018-10-17 23:17:12 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  devServer: {
+    publicPath: '/public',
+    contentBase: path.resolve(__dirname, 'public')
+  }
+};
